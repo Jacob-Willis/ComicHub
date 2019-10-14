1cdbe3ce3c8139a4f30d7e3c9621592e178997f9
Jacob_Willis <jacebly@gmail.com>
2019-10-15 01:15:50 +1100

    Set up more models needed to contain the comic book information and the comic book characters, as well as set up the service to retrieve the online data.

--- ComicHub/ClientApp/src/app/app.module.ts
+++ ComicHub/ClientApp/src/app/app.module.ts
@@ -2,19 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ComicBookInformationService } from './services/comic-book-information.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
     HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    ComicBookInformationService
+  ],
+  bootstrap: [HomepageComponent]
 })
 export class AppModule { }
---
+++ ComicHub/ClientApp/src/app/services/comic-book-information.service.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=comic-book-information.service.js.map
